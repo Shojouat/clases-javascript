9d683f75d00a28c436fe4cfbb5545450a3243d8e
Shojouat <lucas.real296@gmail.com>
2026-05-26 09:26:54 -0300

    Ejercicio 2

---
+++ ejercicios/02-Ejercicio de Objeto.js
@@ -0,0 +1,31 @@
+//Creamos el objeto inventario
+const inventario = {
+    personaje: "Alexia",
+    oro: 100,
+    equipamiento: {
+        arma: "Espada",
+        armadura: "Cuero",
+        accesorio: "Anillos"},
+        //Agregamos el metodo para ver nuestro inventario
+    mostrarInventario() {
+            console.log(`El personaje ${this.personaje} tiene ${this.oro} de oro.
+                Equipamiento:
+                - Arma: ${this.arma}
+                - Armadura: ${this.armadura}
+                - Accesorio: ${this.accesorio}`)}
+}
+//modificamos el valor del oro
+inventario.oro = 50
+//modificamos el arma dentro del equipamiento
+inventario.equipamiento.arma = "Dagas"
+//Desestrucuramos personaje y oro
+const { personaje: personajeInicial, oro: oroInicial } = inventario
+console.log(personajeInicial)
+console.log(oroInicial)
+//Desestructuramos los objetos de equipamiento
+const { equipamiento: {arma, armadura, accesorio}} = inventario
+console.log(arma)
+console.log(armadura)
+console.log(accesorio)
+//Mostramos el metodo
+inventario.mostrarInventario()
